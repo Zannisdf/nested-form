91fe1ccbccfb5c660942b38cb7e744d8be759b5a
Giorgio Zanni <g.zanni@hotmail.com>
2019-01-31 11:02:27 -0300

    FIX: scaffold without form

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_01_31_135320) do
+ActiveRecord::Schema.define(version: 2019_01_31_140026) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
